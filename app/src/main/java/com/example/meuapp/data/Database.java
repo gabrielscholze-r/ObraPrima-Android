be0b9146bbf332f5c005d2ba0f4f40e74074d218
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-06-16 01:30:28 -0300

    created client class

--- app/src/main/java/com/example/meuapp/data/Database.java
+++ app/src/main/java/com/example/meuapp/data/Database.java
@@ -5,7 +5,8 @@ import  com.example.meuapp.data.Profissional;
 public class Database {
 
     public static ArrayList<Profissional> profissionais = new ArrayList<>();
-    public static ArrayList<Pedidos> pedidos = new ArrayList<Pedidos>();
+    public static ArrayList<Pedidos> pedidos = new ArrayList<>();
+    public static ArrayList<Cliente> clientes = new ArrayList<>();
 
 
     public static void loadDatabase(){
@@ -13,9 +14,17 @@ public class Database {
         Profissional pedreiro = new Profissional("Luis","Felipe Scolari","41977777777",1234567891,"Pedreiro",3.0);
         Profissional eletricista = new Profissional("Rogerio","Ceni","4199999999",1234567891,"Eletricista",5.0);
 
+        Cliente cliente1 = new Cliente("Sergio", "sergio123@gmail.com","12345678912","999999999","1234")
+        Cliente cliente2 = new Cliente("Claudio", "claudio123@gmail.com","12345678912","999999999","1234")
+        Cliente cliente3 = new Cliente("Roberto", "roberto123@gmail.com","12345678912","999999999","1234")
+
         profissionais.add(encanador);
         profissionais.add(pedreiro);
         profissionais.add(eletricista);
+
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+        clientes.add(cliente3);
 
         pedidos.add(new Pedidos("05","06","Encanamento Pia", encanador));
         pedidos.add(new Pedidos("10","06","Muro", pedreiro));
@@ -29,5 +38,21 @@ public class Database {
 
     public static ArrayList<Pedidos> getPedidos() {
         return pedidos;
+    }
+
+    public static void setProfissionais(ArrayList<Profissional> profissionais) {
+        Database.profissionais = profissionais;
+    }
+
+    public static void setPedidos(ArrayList<Pedidos> pedidos) {
+        Database.pedidos = pedidos;
+    }
+
+    public static ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public static void setClientes(ArrayList<Cliente> clientes) {
+        Database.clientes = clientes;
     }
 }
