449bda2656cf7c4bb187c720d638e076386f9284
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-06-23 00:49:29 -0300

    id bug

--- app/src/main/java/com/example/meuapp/FormCadastroProfissional.java
+++ app/src/main/java/com/example/meuapp/FormCadastroProfissional.java
@@ -1,7 +1,5 @@
 package com.example.meuapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +9,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.example.meuapp.data.Cliente;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.meuapp.data.Database;
 import com.example.meuapp.data.Profissional;
 
@@ -20,61 +19,53 @@ import java.util.ArrayList;
 public class FormCadastroProfissional extends AppCompatActivity {
 
     private Button bt_cadastrar;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro_profissional);
-
         getSupportActionBar().hide();
         IniciarComponentes();
-
         Spinner ramo = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.ramos_array, R.layout.spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ramo.setAdapter(adapter);
-
-
-
         bt_cadastrar.setOnClickListener(new View.OnClickListener() {
-
-            private ArrayList<Profissional> profissionais = Database.getProfissionais();
+            private final ArrayList<Profissional> profissionais = Database.getProfissionais();
             @Override
             public void onClick(View view) {
                 EditText price = (EditText) findViewById(R.id.edit_price);
-                EditText nome = (EditText)findViewById(R.id.editPersonName);
+                EditText nome = (EditText) findViewById(R.id.editPersonName);
                 String nomeString = nome.getText().toString();
-                EditText email = (EditText)findViewById(R.id.editTextEmailAddress);
+                EditText email = (EditText) findViewById(R.id.editTextEmailAddress);
                 String emailString = email.getText().toString();
-                EditText cpf = (EditText)findViewById(R.id.editTextCPF);
+                EditText cpf = (EditText) findViewById(R.id.editTextCPF);
                 String cpfString = cpf.getText().toString();
-                EditText telefone = (EditText)findViewById(R.id.editTextPhone);
+                EditText telefone = (EditText) findViewById(R.id.editTextPhone);
                 String telefoneString = telefone.getText().toString();
-                EditText senha = (EditText)findViewById(R.id.createPassword);
+                EditText senha = (EditText) findViewById(R.id.createPassword);
                 String senhaString = senha.getText().toString();
-                EditText confirmarSenha = (EditText)findViewById(R.id.confirmPassword);
+                EditText confirmarSenha = (EditText) findViewById(R.id.confirmPassword);
                 String confirmarSenhaString = confirmarSenha.getText().toString();
                 String ramoServicoString = ramo.getSelectedItem().toString();
                 EditText bio = (EditText) findViewById(R.id.editBio);
                 String bioString = bio.getText().toString();
-                if(Database.verificarEmail(emailString)){
-                    TextView error = findViewById(R.id.errorMessage3);
-                    error.setText("Email já cadastrado.");
-                }else if(senhaString.equals(confirmarSenhaString)){
-                    if(nomeString.equals("") || emailString.equals("") || cpfString.equals("") || telefoneString.equals("") ||
-                            senhaString.equals("") || confirmarSenhaString.equals("") || ramoServicoString.equals("") || price.equals("")){
+                if (Database.verificarEmail(emailString)) {
+                    TextView error = findViewById(R.id.errorMessage2);
+                    error.setText("Email já cadastrado");
+                } else if (senhaString.equals(confirmarSenhaString)) {
+                    if (nomeString.equals("") || emailString.equals("") || cpfString.equals("") || telefoneString.equals("") ||
+                            senhaString.equals("") || confirmarSenhaString.equals("") || ramoServicoString.equals("") || price.equals("")) {
                         TextView error = findViewById(R.id.errorMessage2);
                         error.setText("Informações faltando");
-                    } else{
-                        Profissional profissional = new Profissional(nomeString,telefoneString,cpfString,ramoServicoString,0.0,emailString, senhaString,bioString,Double.parseDouble(price.getText().toString()));
+                    } else {
+                        Profissional profissional = new Profissional(nomeString, telefoneString, cpfString, ramoServicoString, 0.0, emailString, senhaString, bioString, Double.parseDouble(price.getText().toString()));
                         profissionais.add(profissional);
                         Database.setProfissionais(profissionais);
                         Intent intent = new Intent(FormCadastroProfissional.this, FormLogin.class);
                         startActivity(intent);
                     }
-                }else{//caso as senhas sejam diferentes
+                } else {//caso as senhas sejam diferentes
                     TextView error = findViewById(R.id.errorMessage2);
                     error.setText("Senhas devem ser iguais");
                 }
@@ -83,7 +74,7 @@ public class FormCadastroProfissional extends AppCompatActivity {
 
     }
 
-    private void IniciarComponentes(){
+    private void IniciarComponentes() {
         bt_cadastrar = findViewById(R.id.bt_cadastrar);
     }
 }
